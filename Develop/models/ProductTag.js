d28aadc0d171dc55ecdbad469393f9c910bd2190
Jonathon <jonathonvanausdal257@gmail.com>
2022-10-06 17:57:54 -0600

    product tag updated

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -15,6 +15,12 @@ ProductTag.init({
         type: DataTypes.INTEGER,
         references: { model: "product", key: "id" },
     },
+    tag_id: {
+        type: DataTypes.INTEGER,
+        references: { model: "tag", key: "id" },
+
+
+    },
 }, {
     sequelize,
     timestamps: false,
