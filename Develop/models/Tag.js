84e65627ccbba39e6ce4001b84db2aaa2762a456
Jonathon <jonathonvanausdal257@gmail.com>
2022-10-06 17:51:01 -0600

    tag model

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -4,17 +4,28 @@ const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
-Tag.init(
-  {
-    // define columns
-  },
-  {
+Tag.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    tag_name: {
+        type: DataTypes.STRING,
+    }
+
+
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'tag',
-  }
-);
+});
 
 module.exports = Tag;
