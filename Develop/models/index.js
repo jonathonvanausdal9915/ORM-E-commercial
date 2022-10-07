652ddc0614a508db53612e46881fc350f6012748
Jonathon <jonathonvanausdal257@gmail.com>
2022-10-06 18:15:35 -0600

    index for models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,16 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+    foreignKey: 'categoryy_id'
+});
 // Categories have many Products
-
+Category.hasMany(Products, {
+    foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, { through: ProductTag });
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag });
 
+module.exports = { User, Workout };
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+    Product,
+    Category,
+    Tag,
+    ProductTag,
 };
