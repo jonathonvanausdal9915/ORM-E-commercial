6771e83faa9da85444854e2850377f9f0b0711f4
Darin VanAusdal <darinv@iMac2017.local>
2022-10-07 17:02:24 -0600

    catergory routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,15 +3,48 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Category.findAll({
+      include: [{
+        model: Product,
+      },],
+    });
+    
+    res.status(200).json({ productData });
+    
+    
+
+    
+  } catch (err) {
+    res.status(500).json(err);
+}
+});
   // find all categories
   // be sure to include its associated Products
-});
 
-router.get('/:id', (req, res) => {
+
+router.get('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const productData = await Category.findByPk(id,{
+      include: [{
+        model: Product,
+
+      },]
+
+    });
+    res.status(200).json({ productData });
+  
+
+
+  } catch (err) {
+    res.status(500).json(err);
+}
+});
   // find one category by its `id` value
   // be sure to include its associated Products
-});
+
 
 router.post('/', (req, res) => {
   // create a new category
