300b1d1c80d54df25070634142abac28e50701cd
Darin VanAusdal <darinv@iMac2015-2.local>
2022-10-08 16:34:31 -0600

    delete route works now

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -127,25 +127,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id',async (req, res) => {
+router.delete('/:id', (req, res) => {
   try {
-    const id = req.params.id;
-    const productData = await Product.findById(id,{
-      include: [{
-        model: Category, Tag,
-
-      },]
-
+    // pass in req.params.id into destroy.
+    const productData = Product.destroy({
+      where: {
+        id: req.params.id
+      }
     });
-    productData.remove(id);
-    res.status(200).json({ productData });
-    
-  
 
-
+    if(!productData) {
+      res.status(404).json({ message: "No product found with that id!"});
+      return;
+    }
+    res.status(200).json(productData,);
   } catch (err) {
     res.status(500).json(err);
-}
+  }
 });
+
 
 module.exports = router;
